4333449422a5561ca2461e1a04b0688a41a603a6
Harry <harikrishnasowreddy14@gmail.com>
2025-03-23 18:56:33 +0530

    added mongoose connection

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = 3000;
 
+dotenv.config();
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // ✅ Fixed env var name
+  .then(() => console.log('MongoDB is connected'))
+  .catch((err) => console.error("MongoDB connection failed", err));
+
+
+app.get('/',(req,res)=> {
+  res.status(200).send('Hey, This is Harikrishna Reddy');
+})
+
+
 // Basic /ping route
 app.get('/ping', (req, res) => {
-  res.status(200).send('Hey, This is Harikrishna Reddy');
+  res.status(200).send('pong');
 });
 
 app.listen(PORT, () => {
